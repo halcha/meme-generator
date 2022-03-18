127ba682c4324fb6c4c4c587bd3ee53278120afe
halcha <sam.hall@sydney.edu.au>
2022-03-19 09:29:03 +1100

    testing mixed content fix

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ var memeListLength, randomMeme, randomQuote, textNode, quotesObject;
 
 //fetch Quotes
 
-async function fetchQuoteList() {
-  // Fetch a random quote from the API
-  const response = await fetch("http://api.quotable.io/quotes?tags=inspirational&limit=75");
-  const data = await response.json();
-  if (response.ok) {
-    // Update array
-    quotesList.push(data);
-    quoteListLength = quotesList[0].results.length + 1;
-  } else {
-    quote.textContent = "An error occured";
-    console.log(data);
-  }
-}
+// async function fetchQuoteList() {
+//   // Fetch a random quote from the API
+//   const response = await fetch("http://api.quotable.io/quotes?tags=inspirational&limit=75");
+//   const data = await response.json();
+//   if (response.ok) {
+//     // Update array
+//     quotesList.push(data);
+//     quoteListLength = quotesList[0].results.length + 1;
+//   } else {
+//     quote.textContent = "An error occured";
+//     console.log(data);
+//   }
+// }
 
 //fetch Memes
 
@@ -42,7 +42,7 @@ async function fetchMemeList() {
   }
 }
 
-fetchQuoteList();
+// fetchQuoteList();
 fetchMemeList();
 
 
@@ -59,11 +59,11 @@ function generateMeme() {
       };
     let randomInt = Math.floor(Math.random() * memeListLength);
     randomMeme = memeList[0].data.memes[randomInt];
-    let randomIntQuote = Math.floor(Math.random() * quoteListLength);
-    randomQuote = quotesList[0].results[randomIntQuote].content;
+    // let randomIntQuote = Math.floor(Math.random() * quoteListLength);
+    // randomQuote = quotesList[0].results[randomIntQuote].content;
     img.src = randomMeme.url;
-    var textNode = document.createTextNode(randomQuote);
-    text.appendChild(textNode);
+    // var textNode = document.createTextNode(randomQuote);
+    // text.appendChild(textNode);
     document.querySelector('section').appendChild(img);
 }
 
